53988ab15425f4637a4ceffa12c6873a242b2b26
Budhshil <128732240+Budhshil@users.noreply.github.com>
2023-09-13 20:29:15 +0530

    Update Que9.py

--- Que9.py
+++ Que9.py
@@ -17,35 +17,4 @@ def solve_sudoku(board):
 
         return True
 
-    def solve():
-        for row in range(9):
-            for col in range(9):
-                if board[row][col] == 0:
-                    for num in range(1, 10):
-                        if is_valid_move(row, col, num):
-                            board[row][col] = num
-                            if solve():
-                                return True
-                            board[row][col] = 0
-                    return False
-        return True
-
-    solve()
-
-# Example usage:
-sudoku_board = [
-    [5, 3, 0, 0, 7, 0, 0, 0, 0],
-    [6, 0, 0, 1, 9, 5, 0, 0, 0],
-    [0, 9, 8, 0, 0, 0, 0, 6, 0],
-    [8, 0, 0, 0, 6, 0, 0, 0, 3],
-    [4, 0, 0, 8, 0, 3, 0, 0, 1],
-    [7, 0, 0, 0, 2, 0, 0, 0, 6],
-    [0, 6, 0, 0, 0, 0, 2, 8, 0],
-    [0, 0, 0, 4, 1, 9, 0, 0, 5],
-    [0, 0, 0, 0, 8, 0, 0, 7, 9]
-]
-solve_sudoku(sudoku_board)
-
-# Print the solved Sudoku board
-for row in sudoku_board:
-    print(row)
+   
